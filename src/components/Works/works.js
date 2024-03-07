b1d1845aec290b424de68e5adc6027909674f1a7
jalenpaige <117240799+jalenpaige@users.noreply.github.com>
2024-03-06 22:26:59 -0800

    works css fix and bronco-swap gh link

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -11,7 +11,7 @@ const Works = () => {
     return (
         <section id='works'>
             <h2 className='worksTitle'>My Portfolio</h2>
-            <span className='worksDesc'>Here of some of the projects I am most proud of! Through them, I have learned a lot about how to code effectivley, both alone and in team situations.</span>
+            <span className='worksDesc'>Here of some of the projects I am most proud of! Through them, I have learned a lot about how to code effectivley, both alone and in team situations. Each image links to a devpost, live-hosted website, or github (with code linked when hovered or on devpost).</span>
             <div className='worksImgs'>
                 <a className="wrapper" href="https://devpost.com/software/project-safe-water" target="_blank" rel="noopener noreferrer">
                 <div class="image-container">
@@ -45,7 +45,8 @@ const Works = () => {
                         <img src={Portfolio4} alt=""/>
                         <div class="image-caption">
                             <h1>Bronco Swap</h1>
-                            <p>A marketplace for university students, designed with Vue3 and Firebase for SCU's 2024 Hack for Humanity.</p>
+                            <p>A marketplace for university students, designed with Vue3 and Firebase for SCU's 2024 Hack for Humanity. Codebase at: </p>
+                            <a className='gh-link' href='https://github.com/calvinwlim/Bronco-Swap/tree/main/Bronco-Swap'>https://github.com/calvinwlim/Bronco-Swap/tree/main/Bronco-Swap</a>
                         </div>
                     </div>
                 </a>
